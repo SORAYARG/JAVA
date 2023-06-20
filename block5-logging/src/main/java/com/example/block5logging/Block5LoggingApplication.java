64e0c57ed666d2318e63e36e9ed15ceec7bc3edd
Soraya Ruiz González <soraya.ruiz@bosonit.com>
2023-06-20 10:07:55 +0200

    add logging

---
+++ block5-logging/src/main/java/com/example/block5logging/Block5LoggingApplication.java
@@ -0,0 +1,37 @@
+package com.example.block5logging;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class Block5LoggingApplication implements CommandLineRunner {
+
+	Logger logger = LoggerFactory.getLogger(Block5LoggingApplication.class);
+
+
+	public static void main(String[] args) {
+		SpringApplication.run(Block5LoggingApplication.class, args);
+	}
+
+
+	@Override
+	public void run(String... args) throws Exception {
+
+
+		logger.warn("Esto es un mensaje de advertencia");
+		logger.info("Esto es un mensaje informativo");
+		logger.error("Esto es un mensaje de error");
+		logger.debug("Este es un mensaje de depuración");
+		logger.trace("Este es un mensaje de traza");
+
+
+
+	}
+
+
+
+}
